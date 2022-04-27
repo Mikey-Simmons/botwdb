4ef05276dc4943fd68a31d4f3d4dd9dbd871de62
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-04-27 01:27:17 -0700

    monsters

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 import {Router} from "@reach/router";
 import Random from "./views/Random";
-import Test1 from "./views/testing";
+import Home from "./views/HomePage";
+import Monsters from "./views/Monsters";
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -11,8 +12,9 @@ var myRandomNum = getRandomInt(387);
 const App = () => {
   return (
     <Router>
-      <Test1 path= "/" />
+      <Home num={myRandomNum} path= "/" />
       <Random num={myRandomNum} path="/random"/>
+      <Monsters path="/monsters"></Monsters>
     </Router>
   );
 };
