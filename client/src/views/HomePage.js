d4861f9076fcd60a3a7458ada0c636b32e4e0182
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-05-09 15:16:30 -0700

    changedHomePage

--- client/src/views/HomePage.js
+++ client/src/views/HomePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import {Link} from "@reach/router";
 import NavBar from "../components/navbar";
 import {RotateCircleLoading} from 'react-loadingg';
+import botwPic from "../botw.png";
 function Random({ num }) {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(false);
@@ -20,19 +21,8 @@ function Random({ num }) {
   
   if(error) return <pre>{JSON.stringify(error,null, 2)}</pre>;
   if(!data) return null;
-  console.log(data.data.name)
      {
-      return (
-        <div className="App">
-          
-          <h1>{data.data.name.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())))}</h1>
-          <img src={data.data.image}></img>
-          <p>{data.data.description}</p>
-          <h3>{data.data.common_locations.join(', ')}</h3>
-          
-          <footer>No. {data.data.id}</footer>
-        </div>
-      )
+      
     }
     return (
       <div className="App">
@@ -45,8 +35,15 @@ function HomePage(){
       
         <div className="App">
           <NavBar></NavBar>
-<h1>Welcome to BOTWDB</h1>
-
+          <div className="home"> 
+          <img className="homepic" src={botwPic}></img>
+          </div>
+          <div className="home">
+<h1>Welcome to BOTWDB!</h1>
+</div>
+<div className="home">
+<p>Here you can find all the data from the popular Nintendo Switch game: "The Legend of Zelda: Breath of the Wild".  This application was created using the Hyrule Compendium API.   </p>
+</div>
 
         </div>
     )
