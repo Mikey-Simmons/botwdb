15db637330a662a8ed71be15f1d83b54ede554ba
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-06-09 17:34:55 -0700

    server_stuff+

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-    
-    mongoose.connect('mongodb://localhost/mybotwdb, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => console.log("DB connection established"))
-        .catch(err => console.log("Something went wrong: ", err));
+mongoose.connect("mongodb://localhost/botwdb_db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("Established a connection to the database!!!"))
+    .catch(err => console.log("Something went wrong when connecting to the database!!!", err));
